342cea313ffb7b7d09b85bb425fb5f65e6cfaa11
sc-pulgan <38398757+sc-pulgan@users.noreply.github.com>
2019-10-03 15:56:08 +0200

    init project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Creature myCreature = new Creature(1,30,"Michou", 10,12);
+        Creature ennemyCreature = new Creature(1,200000,"Patrick", 80,10);
+        Fight fight = new Fight(myCreature, ennemyCreature);
+        System.out.println(fight.startfight());
+    }
+
+
+    public Creature generateCreature(Creature myCreature)
+    {
+
     }
 }
